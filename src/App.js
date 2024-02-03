ec56ee62700abbcd325495a96f2ec4b682416c3e
suraj-narayan <surajnarayan_2k19co397@dtu.ac.in>
2024-02-03 23:52:16 +0530

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import './styles/App.scss';
+import './styles/Header.scss';
+import './styles/Home.scss';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
