ce5e85bf22916c5883738cc00d0169c984c09f7c
suraj-narayan <surajnarayan_2k19co397@dtu.ac.in>
2024-02-07 18:26:29 +0530

    files added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import vg from "../assets/asta.webp"
 
 function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <React.Fragment>
+            <div className = "home">
+                <main>
+                    <h1>SushiStream</h1>
+                    <p>Place for all AnimeBingers!</p>
+                </main>
+            </div>
+            <div className="home2">
+                <img src={vg} alt="Graphics" />
+                <p>
+                    
+                Our library of anime shows is constantly growing, with new titles added regularly. Whether you're a long-time fan or just starting out, you're sure to find something you'll love on SushiStream. In addition to our extensive library of anime shows, SushiStream offers a variety of features that make it the perfect place to watch anime. Our user-friendly interface makes it easy to find and watch your favorite shows, and our chat features allow you to connect with other fans and discuss your favorite moments.
+
+                </p>
+
+            </div>
+        </React.Fragment>
     )
 }
 
